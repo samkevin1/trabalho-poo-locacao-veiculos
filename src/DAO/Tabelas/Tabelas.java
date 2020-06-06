52e09728e3399f45d3f39366479a5ebb93140c34
André Rebonato <andrerebonato@users.noreply.github.com>
2020-06-06 00:05:28 -0300

    Adicionado classe estatica com nome das tabelas

--- src/DAO/Tabelas/Tabelas.java
+++ src/DAO/Tabelas/Tabelas.java
@@ -2,9 +2,9 @@ package DAO.Tabelas;
 
 public class Tabelas {
     public static final String automovel = "automovel";
-    private static final String cliente = "cliente";
-    private static final String endereco = "endereco";
-    private static final String locacao = "locacao";
-    private static final String marca = "marca";
-    private static final String modelo = "modelo";
+    public static final String cliente = "cliente";
+    public static final String endereco = "endereco";
+    public static final String locacao = "locacao";
+    public static final String marca = "marca";
+    public static final String modelo = "modelo";
 }
